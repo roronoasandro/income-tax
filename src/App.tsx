ff422be30b45ea8bfaba4fa9a7a711f18f1c6115
roronoasandro <roronoasandro@gmail.com>
2023-08-16 22:10:38 +0700

    init ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+let name: string = "Sandro";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  const [income, setIncome] = useState('');
+  const [text, setText] = useState('');
+  const [incomeType, setIncomeType] = useState<String>('annualy');
+
+  // This function will be triggered when a radio button is selected
+  const radioHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIncomeType(event.target.value);
+  };
+
+  const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    // Preventing the page from reloading
+    event.preventDefault();
+
+    // Do something
+    let incomeTemp:number = parseInt(income);
+
+    if (isNaN(incomeTemp)) {
+      alert("Is not Number");
+    } else {
+      if(incomeType === "annualy") {
+        alert(income);
+      } else {
+        alert(incomeTemp * 12);
+      }
+    }
+    setIncome('');
+  }
+
+  return <div className='App'>
+    <h1>Income Tax</h1>
+    <form className='myForm' onSubmit={submitForm}>
+    <div className="container">
+      <fieldset>
+        <legend>Please select your income?</legend>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <input
+            type="radio"
+            name="drink"
+            value="annualy"
+            checked={incomeType === 'annualy'}
+            id="annualy"
+            onChange={radioHandler}
+          />
+          <label htmlFor="annualy">Annualy</label>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>
+          <input
+            type="radio"
+            name="drink"
+            value="monthly"
+            checked={incomeType === 'monthly'}
+            id="monthly"
+            onChange={radioHandler}
+          />
+          <label htmlFor="monthly">Monthly</label>
+        </p>
+      </fieldset>
     </div>
-  );
+      <input
+        value={income}
+        onChange={(e) => setIncome(e.target.value)}
+        type='text'
+        className='input-income'></input>
+      <button type='submit'>Submit</button>
+    </form>
+  </div>
 }
 
 export default App;
